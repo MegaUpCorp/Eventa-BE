f3fb5d48e10398c73e840ad810821bf7cf75649c
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2025-02-17 21:36:34 +0700

    come to IOrganizer

---
+++ Eventa/Eventa_BusinessObject/Entities/Organizer.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventa_BusinessObject.Entities
+{
+    public class Organizer : BaseEntity
+    {
+        [BsonElement("organizerId")]
+        [BsonRepresentation(MongoDB.Bson.BsonType.String)]
+        public Guid OrganizerId { get; set; } = Guid.NewGuid();
+        [BsonRepresentation(BsonType.String)]
+        [BsonElement("accountId")]
+        public Guid AccountId { get; set; }
+        [Required]
+        [BsonElement("organizerName")]
+        public required string OrganizerName { get; set; }
+        [BsonElement("organizerDescription")]
+        public string? OrganizerDescription { get; set; }
+
+    }
+}
---
+++ Eventa/Eventa_Services/Interfaces/IOrganizerService.cs
@@ -0,0 +1,20 @@
+﻿using Eventa_BusinessObject.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventa_Services.Interfaces
+{
+    public interface IOrganizerService
+    {
+        Task<ActionResult<Organizer?>> GetOrganizerByAccountId(Guid accountId);
+        Task<ActionResult<List<Organizer>>> GetAllOrganizer();
+        Task<ActionResult<Organizer?>> GetOrganizerById(Guid id);
+        Task<bool> AddOrganizer(Organizer organizer);
+        Task<bool> UpdateOrganizerById(Organizer organizer);
+        Task<bool> DeleteOrganizerById(Guid id);
+    }
+}
