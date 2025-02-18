0c545d4c6cbed26ea8f784736fafe922a565e32c
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2025-02-18 16:15:28 +0700

    completed Organizer

---
+++ Eventa/Eventa_BusinessObject/DTOs/Organizer/UpdateOrganizerDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventa_BusinessObject.DTOs.Organizer
+{
+    public class UpdateOrganizerDTO
+    {
+        public string? OrganizerName {  get; set; }
+        public string? OrganizerDescription { get; set; }
+    }
+}
