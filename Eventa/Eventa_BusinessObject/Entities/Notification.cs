cf8eef3e09e9f705b0435ad75b426a47755667dd
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2025-02-22 22:49:10 +0700

    notification but wrong

---
+++ Eventa/Eventa_BusinessObject/Entities/Notification.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventa_BusinessObject.Entities
+{
+    public class Notification : BaseEntity
+    {
+        [BsonRepresentation(BsonType.String)]
+        [BsonElement("accountId")]
+        public Guid AccountId { get; set; }
+        [BsonElement("message")]
+        public string? Message { get; set; }
+        [BsonElement("isRead")]
+        public bool IsRead { get; set; }
+    }
+}
